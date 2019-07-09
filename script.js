a906d1d70f9963e9473045b624326abf06bdf37e
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-07-08 22:43:22 -0400

    adding css animation js and buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+// var video = document.querySelector("video");
+// document.body.classList.add('js-loading');
+// video.addEventListener("load", removeLoadingClass);
+// function removeLoadingClass() {
+//   document.body.classList.remove('js-loading');
+// }
